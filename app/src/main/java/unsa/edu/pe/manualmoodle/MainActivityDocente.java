99b94fdf0c8effcc9f96f2aa32d16491693ff6b9
Gohan-PC <nahoj1992@gmail.com>
2018-07-16 21:49:41 -0500

    primer tuto

--- app/src/main/java/unsa/edu/pe/manualmoodle/MainActivityDocente.java
+++ app/src/main/java/unsa/edu/pe/manualmoodle/MainActivityDocente.java
@@ -2,13 +2,36 @@ package unsa.edu.pe.manualmoodle;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivityDocente extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_docente);
+
+        tabLayout = findViewById(R.id.tablayout_id_docente);
+        viewPager = findViewById(R.id.viewpager_id_docente);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        //Agregamos los fragment
+        adapter.AddFragment(new FragmentVideo(),"");
+        adapter.AddFragment(new FragmentPdf(), "");
+
+        //lea la vista
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        //agregar dibujo al tab
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_video_library);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_picture_as_pdf);
+
     }
 }
--- app/src/main/java/unsa/edu/pe/manualmoodle/MainActivityEstudiante.java
+++ app/src/main/java/unsa/edu/pe/manualmoodle/MainActivityEstudiante.java
@@ -2,13 +2,35 @@ package unsa.edu.pe.manualmoodle;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivityEstudiante extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_estudiante);
+
+        tabLayout = findViewById(R.id.tablayout_id_estudiante);
+        viewPager = findViewById(R.id.viewpager_id_estudiante);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        //Agregamos los fragment
+        adapter.AddFragment(new FragmentVideo(),"");
+        adapter.AddFragment(new FragmentPdf(), "");
+
+        //lea la vista
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        //agregar dibujo al tab
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_video_library);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_picture_as_pdf);
     }
 }
