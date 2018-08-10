184a0714287e0391b4c9c7a53cda2651323105bd
Gohan-PC <nahoj1992@gmail.com>
2018-08-10 01:55:47 -0500

    Version final del manual moodle 1.2.1 (agregando progressbar aula virtyal)

--- app/src/main/java/unsa/edu/pe/manualmoodle/MainActivityAulaVirtual.java
+++ app/src/main/java/unsa/edu/pe/manualmoodle/MainActivityAulaVirtual.java
@@ -1,15 +1,25 @@
 package unsa.edu.pe.manualmoodle;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivityAulaVirtual extends AppCompatActivity {
 
     private WebView aula;
-
+    private ProgressBar pbar;
+    private Handler handler;
+    private Runnable runnable;
+    private Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +29,26 @@ public class MainActivityAulaVirtual extends AppCompatActivity {
         aula.setWebViewClient(new WebViewClient());
         aula.loadUrl("http://dutic.unsa.edu.pe/aulavirtual/");
         aula.getSettings().setJavaScriptEnabled(true);
+
+        pbar = findViewById(R.id.progresbar);
+        pbar.setVisibility(View.VISIBLE);
+
+        handler = new Handler();
+        runnable = new Runnable(){
+
+            @Override
+            public void run() {
+                pbar.setVisibility(View.GONE);
+                timer.cancel();
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(runnable);
+            }
+        },5000,5000);
     }
 }
