4d88f6cba6e897116557770900b140de2d933151
Gohan-PC <nahoj1992@gmail.com>
2018-08-10 01:03:21 -0500

    Version final del manual moodle 1.2 (agregando agregando notificacion de actualizacion)

--- app/src/main/java/unsa/edu/pe/manualmoodle/MainActivity.java
+++ app/src/main/java/unsa/edu/pe/manualmoodle/MainActivity.java
@@ -1,5 +1,12 @@
 package unsa.edu.pe.manualmoodle;
 
+import android.app.PendingIntent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
@@ -15,10 +22,42 @@ public class MainActivity extends AppCompatActivity {
     private ImageView img;
     private TextView texto;
     private Button btnEstudiante, btnDocente, btnAulavirtual;
+
+    NotificationCompat.Builder mNotificaBuider;
+    int notificationid = 3415;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PackageInfo pinfo = null;
+        try {
+            pinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        int versionCode = pinfo.versionCode;
+        String versionName = pinfo.versionName;
+        if (versionCode != versionCode && versionName != versionName){
+
+            Uri webpage = Uri.parse("https://play.google.com/store/apps/details?id=unsa.edu.pe.manualmoodle");
+            Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            mNotificaBuider = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.drawable.logodutic)
+                    .setContentTitle("Manual Moodle")
+                    .setContentText("Hay una actualizacion del Manual")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    // Set the intent that will fire when the user taps the notification
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(notificationid,mNotificaBuider.build());
+        }
+
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher_round);
