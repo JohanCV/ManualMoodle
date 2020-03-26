19514f6190b1b4838141dfde1577f6485e8e357a
Gohan <nahoj1992@gmail.com>
2020-03-26 17:16:34 -0500

    actualizacion 3.5 (version de app remote config)

--- app/src/main/java/unsa/edu/pe/manualmoodle/MainActivity.java
+++ app/src/main/java/unsa/edu/pe/manualmoodle/MainActivity.java
@@ -1,6 +1,8 @@
 package unsa.edu.pe.manualmoodle;
 
 import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
@@ -15,19 +17,25 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements UpdateHelper.OnUpdateCheckListener {
-    private View view;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
     private ImageView img;
     private TextView texto;
     private Button btnEstudiante, btnDocente, btnAulavirtual;
-
+    private int version;
+    private FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher_round);
@@ -45,7 +53,45 @@ public class MainActivity extends AppCompatActivity implements UpdateHelper.OnUp
         btnDocente.startAnimation(myanimacion);
         btnAulavirtual.startAnimation(myanimacion);
 
-        UpdateHelper.with(this).OnUpdateCheck(this).check();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        PackageInfo packageInfo;
+
+        try {
+            packageInfo = this.getPackageManager().getPackageInfo(getPackageName(),0);
+            version = packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        remoteConfig.setConfigSettings(new FirebaseRemoteConfigSettings.Builder().setDeveloperModeEnabled(true).build());
+        HashMap<String,Object> actualizacion = new HashMap<>();
+        actualizacion.put("versioncode",version);
+
+        Task<Void> fetch= remoteConfig.fetch(0);
+        fetch.addOnSuccessListener(this, new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                remoteConfig.activateFetched();
+                version(version);
+            }
+        });
+    }
+
+    private void version(int version){
+        int nueva = (int) remoteConfig.getLong("versioncode");
+        String web=  remoteConfig.getString("web");
+        String versionname = remoteConfig.getString("versionname");
+
+        //version = la version del celular q tiene
+        if (nueva > version){
+            dialogActualizacionVersion(web,versionname);
+            //Toast.makeText(MainActivity.this, "Existe una nueva Version!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     /** llamando al btn sendMEstudiante */
@@ -64,30 +110,43 @@ public class MainActivity extends AppCompatActivity implements UpdateHelper.OnUp
                 i = new Intent(this, MainActivityAulaVirtual.class);
         }
         startActivity(i);
-    }
+    }//fin de ClickMe
 
+    public void dialogActualizacionVersion(final String url, String versionName){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        View vista = getLayoutInflater().inflate(R.layout.dialog_actualizacion,null);
 
+        TextView versiontxt = vista.findViewById(R.id.tv_version_name);
+        Button cancelar, actualizar;
+        cancelar = vista.findViewById(R.id.buttonCancel);
+        actualizar = vista.findViewById(R.id.buttonActualizar);
 
+        versiontxt.setText(versionName);
 
-    @Override
-    public void onUpdateCheckListener(final String urlApp) {
-        AlertDialog dialog = new AlertDialog.Builder(this)
-                .setTitle("New version available")
-                .setMessage("Please, update app to new version to continue reposting.")
-                .setPositiveButton("Update",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Toast.makeText(MainActivity.this,""+urlApp,Toast.LENGTH_SHORT).show();
-                            }
-                        }).setNegativeButton("No, thanks",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        }).create();
+        dialogBuilder.setCancelable(false);
+        dialogBuilder.setView(vista);
+        final AlertDialog dialog = dialogBuilder.create();
         dialog.show();
+
+
+        cancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this, "Te perderas Novedades de la APP!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        actualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent abrirUrl  = new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+                startActivity(abrirUrl);
+            }
+        });
+
+
+
     }
 
 }
---
+++ app/src/main/java/unsa/edu/pe/manualmoodle/MyFirebaseMessageService.java
@@ -0,0 +1,37 @@
+package unsa.edu.pe.manualmoodle;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessageService extends FirebaseMessagingService {
+
+    public static final String TAG="NOTICIA: ";
+
+    public MyFirebaseMessageService() {Log.d(TAG, "From: ");
+    }
+
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        Log.d(TAG, "Refreshed token: " + token);
+    }
+}
